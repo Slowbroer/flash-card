480626f95eb4f3543fb4b1fc4d9b3976e0a7ad3b
Slowbro <1573975217@qq.com>
2021-04-09 01:07:33 +0800

    fixed

--- app/model/user.py
+++ app/model/user.py
@@ -6,9 +6,9 @@ from app import db
 
 class Users(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    union_id = db.Column(db.String(50), unique=True, nullable=False)
+    union_id = db.Column(db.String(50), unique=True, nullable=True)
     open_id = db.Column(db.String(50), unique=True, nullable=False)
-    name = db.Column(db.String(50), nullable=False)
+    name = db.Column(db.String(50), nullable=True)
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, server_default=db.func.now(), server_onupdate=db.func.now())
 
