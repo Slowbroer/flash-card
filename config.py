5678e21ee62dbfe38d75ec34d319c8d88a04fd62
Slowbro <1573975217@qq.com>
2021-04-09 17:23:59 +0800

    add SECRET_KEY config

--- config.py
+++ config.py
@@ -7,8 +7,8 @@ class Config(object):
 
     APP_ENV = ''
     DEBUG = False
-    SECRET_KEY = ''  # TODO
     JWT_AUTH_USERNAME_KEY = 'code'
+    SECRET_KEY = os.environ.get('SECRET_KEY')
     # JWT_AUTH_URL_RULE = None
     WE_CHAT_APP_ID = os.environ.get('WE_CHAT_APP_ID')
     WE_CHAT_SECRET = os.environ.get('WE_CHAT_SECRET')
