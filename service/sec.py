75b786de4a7ade700713312b7237096cd7a3a08a
Slowbro <1573975217@qq.com>
2021-04-28 00:29:54 +0800

    fixed

--- service/sec.py
+++ service/sec.py
@@ -17,9 +17,10 @@ class SecCheck(object):
         if token is None:
             return False
         app.logger.info(token)
-        token = str(token, encoding="utf-8")
+        app.logger.info(f"https://api.weixin.qq.com/wxa/msg_sec_check?access_token={token}&content={content}")
         content = requests.post(
-            url=f"https://api.weixin.qq.com/wxa/msg_sec_check?access_token={token}&content={content}",
+            url=f"https://api.weixin.qq.com/wxa/msg_sec_check?access_token={token}",
+            json={"content": content}
         ).content
         app.logger.info(content)
         content_obj = json.loads(content)
@@ -29,6 +30,7 @@ class SecCheck(object):
 
     def __token(self):
         token = redis_client.get("wc_token")
+        app.logger.info(token)
         if token is None:
             content = requests.get(f"https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential"
                                    f"&appid={self.app_id}&secret={self.secret}").content
@@ -39,5 +41,7 @@ class SecCheck(object):
                 redis_client.set("wc_token", token, content_obj['expires_in'])
             else:
                 return None
+        else:
+            token = str(token, "utf-8")
         return token
 
